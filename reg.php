b2dda58ecfdef3ce1b1d2f858589d0db3973de40
vot3tada <zindromed@bk.ru>
2022-02-13 22:51:16 +0300

    test3

--- reg.php
+++ reg.php
@@ -1,22 +1,49 @@
 <?php
+    $l = "";
     require("header.php");
-?>
-<div class="cover-container w-100 h-100 p-3 mx-auto flex-column">
-<h1 class="text-center text-primary">Веб-технологии</h1>
-<main class="px-3 abober"> 
+    require("db.php");
     
-    <form action="reg.php" method="post">
+    if (isset($_REQUEST['sub']))
+    {
+        $login = $_REQUEST['login'];
+        $password = $_REQUEST['password'];
+
+
+        $stmt = $db->prepare("SELECT * FROM `users` WHERE `login` = ?");
+        $stmt->execute([$login]);
+        $category = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($category == null)
+        {
+            
+            $query = "INSERT INTO `users` (`login`, `password`) VALUES (:login, :password);";
+            $params = [
+                ':login' => $login,
+                ':password' => $password
+            ];
+            $stmt = $db->prepare($query);
+            $stmt->execute($params);
+        }
+        else
+        {
+            $l = "Этот логин занят";
+        }
+    }
+?>
+    <div class="cover-container w-100 h-100 p-3 mx-auto flex-column">
+<main class="px-3 abober"> 
+    <form action="reg.php" method="POST">
     <div class="aboba form-group p-5">
-    <input type="text" required placeholder="Логин" id="login" class="form-control mb-2 mr-sm-2">
-    <input type="password" required placeholder="Пароль" id="password" class="form-control mb-2 mr-sm-2">
-    <button type="submit" class="btn btn-primary mb-2">Регистрация</button>
+    <input type="text" required placeholder="Логин" name="login" class="form-control mb-2 mr-sm-2">
+    <input type="password" required placeholder="Пароль" name="password" class="form-control mb-2 mr-sm-2">
+    <button type="submit" name="sub" class="btn btn-primary mb-2">Регистрация</button>
+    <?php
+        print("<p class='text-primary'>".$l."</p>");
+    ?>
     </div>
     </form>
     </main>
-
     </div>
-    
-
 <?php
     require("footer.php");
 ?>
