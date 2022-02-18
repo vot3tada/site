d9d96b090dcda28adff631d0f31b893ba8dd3d0d
vot3tada <zindromed@bk.ru>
2022-02-18 21:45:42 +0300

    updatebase

--- data.sql
+++ data.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Фев 17 2022 г., 21:57
+-- Время создания: Фев 18 2022 г., 19:45
 -- Версия сервера: 10.3.16-MariaDB
 -- Версия PHP: 7.3.6
 
@@ -36,6 +36,19 @@ CREATE TABLE `exercises` (
   `stage` int(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Дамп данных таблицы `exercises`
+--
+
+INSERT INTO `exercises` (`id`, `user`, `name`, `description`, `stage`) VALUES
+(35, 'votetada', 'Абоба', '-Купить абобу\r\n-Удалить абобу\r\n-Иван привет\r\n-хочу кушац\r\n', 1),
+(36, 'votetada', 'Тренировка', 'биг дата\r\nшоу юр мотивэшион', 2),
+(42, 'votetada', 'Че-нибудь', 'Купить хлеб и масло, вытряхнуть палас', 0),
+(44, 'Aboba', 'Я второй пользователь', 'Хочу купить горилки', 0),
+(46, 'votetada', 'Карточка', 'Карта Я карта', 0),
+(47, 'votetada', 'Дивизия', 'Моя голова стального ведра,\r\nЯ слишком давно стою вот так.\r\nМасленка как знак, в девичьих руках\r\nЯ кажется вляпался как тогда\r\nИ все это бред, и сердца нет\r\nСоломенный дурень сгорит хохоча\r\nНо я не могу, я просто уйду\r\nПо дороге из желтого кирпича…\r\n', 2),
+(49, 'votetada', 'Скрипты не работают', '&#60;script&#62;alert(\"Привет\");&#60;/script&#62;\r\n&#60;script&#62;alert(\"ТУТУТУ\");&#60;/script&#62;', 0);
+
 -- --------------------------------------------------------
 
 --
@@ -52,6 +65,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`login`, `password`) VALUES
+('Aboba', '12345'),
 ('admin', 'admin'),
 ('votetada', '1234');
 
@@ -80,7 +94,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `exercises`
 --
 ALTER TABLE `exercises`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
