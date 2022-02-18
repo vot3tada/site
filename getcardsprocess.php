17a4e1f8822301ae2e0f8305202cc2c20f1b23e8
vot3tada <zindromed@bk.ru>
2022-02-18 21:13:56 +0300

    getcardrowfile

--- getcardsprocess.php
+++ getcardsprocess.php
@@ -3,22 +3,5 @@
     $stmt->execute([$_SESSION['login']]);
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
       {
-        print("<form action='main.php' method='POST'>
-        <div class='card cardab'>
-        <div class='card-header' style='display: inline-flex;justify-content: space-between;'>
-            <h4 class='text-primary'>".$row['name']."</h4>
-            <div>
-            <button type='submit' name='edit' value='".$row['id']."' class='btn btn-warning'>Edit</button>
-            <button type='submit' name='delete' value='".$row['id']."' class='btn btn-danger'>X</button>
-            </div>
-        </div>
-        <div class='card-body'>
-            <textarea name='description' cols='40' rows='10' style='resize: none;' class='text-secondary form-control'>".$row['description']."</textarea>
-            <div style='display: grid;grid-auto-flow: column;'>
-            <button type='submit' name='left' value='".$row['id']."' class='btn btn-primary'><</button>
-            <button type='submit' name='right' value='".$row['id']."' class='btn btn-primary'>></button>
-            </div>
-        </div>
-        
-        </div></form>");
+        require("getcardrow.php");
       }
